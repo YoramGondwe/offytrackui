226aab6403a05082c2b8f41409a75a4adc53aa53
yoram-g <yoramgondwe3rd@gmail.com>
2021-03-21 05:34:53 +0200

    👍

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>,
+  // <QueryClientProvider client={queryClient}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // {/* </QueryClientProvider> */}
   document.getElementById("root")
 );
 
